a877ea28c6d82588a49cd8c15afc67bce85ae1ab
Koki Yamaguchi <koki@laptopkoki>
2024-01-21 13:12:29 -0800

    implement createGroup page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Flex, Container, Box, Text, Heading, UnorderedList, ListItem } from '@chakra-ui/react'
+import NavBar from '../components/NavBar'
 
 const About = () => {
   return (
     <div>
+      <NavBar/>
       <Flex minH={1} direction="column" align="center" justify="center">
+        
         <Container maxW="container.md">
           <Box bg="gray.200" p={4} borderRadius="md" boxShadow="md">
-            <Text fontSize="xl" fontWeight="bold" mb={4}>
+            <Text fontSize="xl" fontWeight="bold" mb={5}>
               Welcome to FocusMate!
             </Text>
             <Text fontSize="lg" mb={4}>
               In this website, you can join and find the projects that you are interested in.
             </Text>
-            <Heading as="h3" size="md" mb={4}>
+            <Heading as="h3" size="md" mb={4} >
               Benefits of working on projects:
             </Heading>
             <UnorderedList>
